18521e77ba8ee88cb40aae85e77af74d86293218
gulechakan <65244180+gulechakan@users.noreply.github.com>
2025-10-03 16:43:09 -0400

    Update profile.py

--- profile.py
+++ profile.py
@@ -19,8 +19,7 @@ request = pc.makeRequestRSpec()
  
 # Add a raw PC to the request.
 node = request.RawPC("node")
-
-print("meghana")
+node.hardware_type = "d430"
 
 # Install and execute a script that is contained in the repository.
 node.addService(pg.Execute(shell="sh", command="/local/repository/silly.sh"))
